48e8a3909a678c48f3c712017a2f2d1833dd203b
Aniket <aniket.tatipamula@gmail.com>
2020-04-21 12:58:23 +0530

    Read  Webcam video From Browser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super()
+    
+    // Create A refrence for DOM video
+    this.localVideoref = React.createRef()
+  }
+
+  render(){
+    // create constriants for data to be accessed
+    // in our case its only webcam so video
+    const constriants = { video: true }
+    
+    // Method Called when User media is received
+    const success = (stream) => {
+      this.localVideoref.current.srcObject = stream
+    }
+    
+    // Method to show error while requesting user media
+    const failure = (e) => {
+      console.log('getUserMedia Error', e)
+    }
+    
+    // Access User media using promise
+    // this method asks for user permission on browser 
+    navigator.mediaDevices.getUserMedia( constriants )
+      .then( success )
+      .catch( failure )
+
+    return (
+      <div>
+       <h1>Read Webcam Video Data!</h1>
+       <video ref={this.localVideoref} autoPlay></video>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
